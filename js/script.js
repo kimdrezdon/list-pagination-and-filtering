5ccda9aab0d14c46ad8433c9b7c8642841e0b776
Kim Drezdon <kdrezdon@gmail.com>
2019-02-02 20:19:00 -0600

    Replaced comments with my own

--- js/script.js
+++ js/script.js
@@ -2,38 +2,11 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const pageDiv = document.querySelector('div.page');
 const studentList = document.querySelectorAll('li.student-item');
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//function used to display only ten students based on the page selected
 
-   Pro Tips: 
-   - Keep in mind that with a list of 54 students, the last page 
-      will only display four.
-   - Remember that the first student has an index of 0.
-   - Remember that a function `parameter` goes in the parens when 
-      you initially define the function, and it acts as a variable 
-      or a placeholder to represent the actual function `argument` 
-      that will be passed into the parens later when you call or 
-      "invoke" the function 
-***/
 const showPage = (list, page) => {
    const firstIndex = (page * 10) - 10;
    const lastIndex = (page * 10) - 1;
@@ -46,9 +19,10 @@ const showPage = (list, page) => {
    };
 }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+/***
+   Function used to create the correct number of page links based on the
+   total number of students in a list, and call the showPage function when
+   each link is clicked
 ***/
 
 const appendPageLinks = (list) => {
@@ -79,14 +53,13 @@ const appendPageLinks = (list) => {
          };
          const activePage = e.target;
          activePage.className = 'active';
-         const pageNum = i + 1;
-         showPage(list, pageNum);
+         const activePageNum = i + 1;
+         showPage(list, activePageNum);
       });
    }
-
 }
 
+//Call the showPage function for the initial load to display page 1
 showPage(studentList, 1);
-appendPageLinks(studentList);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(studentList);
